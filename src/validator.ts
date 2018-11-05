d7b6a60655c434a7902e6444dbd5125d3f56f540
KAREN <lafuentekaren@gmail.com>
2018-11-05 14:08:59 -0300

    Added notUppercase, notVarargAsLastParameter problems and tests

--- src/validator.ts
+++ src/validator.ts
@@ -6,3 +6,43 @@
 // No asignation of fully qualified references
 // No references named as keywords
 // No try without catch or always
+import { Node, Parameter, reduce } from './model'
+
+
+// agrego el enum porque no me reconocía el level cuando lo ponía como 'Warning'  | 'Error
+export const enum level { warning, error }
+const isUpperCase = (s: string) => /^[A-Z]$/.test(s)
+const findFirstVararg = (parameter: Parameter) => parameter.isVarArg
+
+export interface Problem {
+  readonly code: string
+  readonly level: level
+  readonly node: Node
+}
+
+export default (node: Node): ReadonlyArray<Problem> =>
+
+  // tslint:disable-next-line:no-shadowed-variable
+  reduce<Problem[]>((problems, node) => {
+    switch (node.kind) {
+      case 'Class':
+        const isNonUpperCase = isUpperCase(node.name[0]) ? [] : [{
+          code: 'notUppercase',
+          level: level.warning,
+          node,
+        }]
+        return [...problems, ...isNonUpperCase]
+
+      case 'Method':
+        // tslint:disable-next-line:triple-equals
+        const lastParameterVararg = node.parameters.findIndex(findFirstVararg) + 1 == (node.parameters.length) ? [] : [{
+          code: 'notVarargAsLastParameter',
+          level: level.error,
+          node,
+        }]
+        return [...problems, ...lastParameterVararg]
+
+      default:
+        return problems
+    }
+  })([], node)
---
+++ test/validator.test.ts
@@ -0,0 +1,113 @@
+import { should } from 'chai'
+import { Class as ClassNode, Package as PackageNode } from '../src/model'
+import validate, { level } from '../src/validator'
+import { Class, Method, Mixin, Package, Parameter } from './builders'
+
+import link from '../src/linker'
+
+should()
+
+describe('Validator', () => {
+
+  const WRE = Package('wollok')(
+    Class('Object')(),
+    Class('Closure')()
+  )
+
+  const environment = link([
+    WRE,
+    Package('p')(
+      Class('C')(),
+      Package('q')(
+        Mixin('M')()
+      )
+    ),
+  ])
+
+  const environment2 = link([
+    WRE,
+    Package('p')(
+      Class('c')(),
+      Package('q')(
+        Mixin('M')()
+      )
+    ),
+  ])
+
+  const packageExample = environment.members[1] as PackageNode
+  const C = packageExample.members[0] as ClassNode
+  const packageExample2 = environment2.members[1] as PackageNode
+  const C2 = packageExample2.members[0] as ClassNode
+  // const q = p.members[1] as PackageNode
+  // const M = q.members[0] as MixinNode
+
+  describe('Classes', () => {
+    it('Class with first uppercase letter returns empty list of error', () => {
+      validate(C).should.deep.equal([])
+    })
+
+    it('Class with non first uppercase letter returns empty list of error returns notUppercase', () => {
+      validate(C2).should.deep.equal([{
+        code: 'notUppercase',
+        level: level.warning,
+        node: C2,
+      }])
+    })
+
+  })
+  describe('Methods', () => {
+
+    // tslint:disable-next-line:no-shadowed-variable
+    const environment3 = link([
+      WRE,
+      Package('p')(
+        Class('C')(
+          Method('m', {
+            parameters: [Parameter('p'), Parameter('q', {
+              isVarArg: true,
+            })],
+          })()),
+        Package('q')(
+          Mixin('M')()
+        )
+      ),
+    ])
+
+    const environment4 = link([
+      WRE,
+      Package('p')(
+        Class('C')(
+          Method('m', {
+            parameters: [Parameter('c'), Parameter('q', {
+              isVarArg: true,
+            }), Parameter('p')],
+          })()),
+        Package('q')(
+          Mixin('M')()
+        )
+      ),
+    ])
+
+    const packageExample3 = environment3.members[1] as PackageNode
+    const cl = packageExample3.members[0] as ClassNode
+
+    const packageExample4 = environment4.members[1] as PackageNode
+    const cl2 = packageExample4.members[0] as ClassNode
+    const m = cl2.members[0]
+
+    it('Last parameter as vararg returns empty list of error', () => {
+      validate(cl).should.deep.equal([])
+    })
+
+    it('Non Last parameter as vararg returns error', () => {
+      validate(m).should.deep.equal([{
+        code: 'notVarargAsLastParameter',
+        level: level.error,
+        node: m,
+      }])
+    })
+
+  })
+
+
+})
