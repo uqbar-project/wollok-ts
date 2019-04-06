5fa3a6db86a36334a7a49eb721928abe9185e552
PalumboN <nahuel.palumbo@gmail.com>
2019-04-06 19:33:57 -0300

    Adding without extension

--- src/extensions.ts
+++ src/extensions.ts
@@ -27,3 +27,7 @@ export const mapObject = <T, R = any>(tx: (value: T[keyof T], key: keyof T) => R
   }
   return response
 }
+
+export const without = <T>(ignores: List<T>) => (elements: List<T>): List<T> =>
+  elements.filter(it => !ignores.includes(it))
+
