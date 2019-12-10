d16fbdd275e07738821a34275a7dd89bdfb68158
Nicolás Scarcella <nscarcella@gmail.com>
2019-12-10 00:41:06 -0300

    pulling specific language version

--- scripts/fetchLanguage.ts
+++ scripts/fetchLanguage.ts
@@ -2,7 +2,8 @@ import { existsSync, mkdirSync } from 'fs'
 import gitClient from 'simple-git/promise'
 import log, { enableLogs, LogLevel } from '../src/log'
 
-const WOLLOK_LANGUAGE_REPO = `https://github.com/uqbar-project/wollok-language.git#v${process.env.npm_package_wollokVersion}`
+const WOLLOK_LANGUAGE_REPO = `https://github.com/uqbar-project/wollok-language.git`
+const WOLLOK_LANGUAGE_VERSION = process.env.npm_package_wollokVersion
 const WOLLOK_LANGUAGE_FOLDER = 'language'
 
 enableLogs(LogLevel.INFO)
@@ -11,18 +12,12 @@ const fetchLanguage = async () => {
   log.start('Obtaining the Wollok Language specification')
   if (existsSync(WOLLOK_LANGUAGE_FOLDER)) {
     log.info('\tFound local version of Wollok Language!')
-    const diff = await gitClient(WOLLOK_LANGUAGE_FOLDER).diff()
-    if (diff.length) {
-      log.error(`\tCan't pull the Wollok Language project because the local version has uncommited changes. Commit your changes and try again.`)
-      process.exit(-1)
-    }
-
-    log.info(`\tPulling the Wollok Language project to the latest version from ${WOLLOK_LANGUAGE_REPO}`)
-    await gitClient(WOLLOK_LANGUAGE_FOLDER).pull()
   } else {
-    log.info(`\tChecking out the Wollok Language project to ./${WOLLOK_LANGUAGE_FOLDER} from ${WOLLOK_LANGUAGE_REPO}`)
+    log.info(`\tChecking out the Wollok Language project to ./${WOLLOK_LANGUAGE_FOLDER} from ${WOLLOK_LANGUAGE_REPO} at tag: v${WOLLOK_LANGUAGE_VERSION}`)
     mkdirSync(WOLLOK_LANGUAGE_FOLDER)
-    await gitClient(WOLLOK_LANGUAGE_FOLDER).clone(WOLLOK_LANGUAGE_REPO, '.')
+    const client = gitClient(WOLLOK_LANGUAGE_FOLDER)
+    await client.clone(WOLLOK_LANGUAGE_REPO, '.')
+    await client.checkout(`v${WOLLOK_LANGUAGE_VERSION}`)
   }
   log.done('Obtaining the Wollok Language specification')
 }
