8f595b095a0f9ae0fa779b20af36fe0befa33baf
Nicolás Scarcella <nscarcella@gmail.com>
2019-12-20 01:04:52 -0300

    using standard log to make it visible even if not verbose

--- test/wtest.ts
+++ test/wtest.ts
@@ -5,11 +5,12 @@ import { basename, join } from 'path'
 import yargs from 'yargs'
 import { buildEnvironment } from '../src'
 import interpreter, { Evaluation, Natives } from '../src/interpreter'
-import log, { enableLogs, LogLevel } from '../src/log'
+import { enableLogs, LogLevel } from '../src/log'
 import { List, Node } from '../src/model'
 import natives from '../src/wre/wre.natives'
 
 const { fail } = assert
+const { time, timeEnd } = console
 
 const ARGUMENTS = yargs
   .option('verbose', {
@@ -30,46 +31,55 @@ const SKIPPED = globby.sync([
   'game/**',
 ], { cwd: ARGUMENTS.root })
 
+
+function registerTests(evaluation: Evaluation, nodes: List<Node>) {
+  nodes.forEach(node => {
+
+    if (node.is('Describe') || node.is('Package'))
+      describe(node.name, () => registerTests(evaluation, node.members))
+
+    if (node.is('Test'))
+      it(node.name, () => {
+        const { runTest } = interpreter(evaluation.environment, natives as Natives)
+        const { error } = runTest(evaluation.copy(), node)
+        if (error) fail(`${error}`)
+      })
+
+  })
+}
+
+
 describe(basename(ARGUMENTS.root), () => {
 
   if (ARGUMENTS.verbose) enableLogs(LogLevel.DEBUG)
 
-  log.start('Reading tests')
+  time('Reading tests')
+
   const testFiles = globby.sync('**/*.@(wlk|wtest)', { cwd: ARGUMENTS.root })
     .filter(name => !SKIPPED.includes(name))
     .map(name => ({
       name,
       content: readFileSync(join(ARGUMENTS.root, name), 'utf8'),
     }))
-  log.done('Reading tests')
 
-  log.start('Building environment')
+  timeEnd('Reading tests')
+
+
+  time('Building environment')
+
   const environment = buildEnvironment(testFiles)
-  log.done('Building environment')
+
+  timeEnd('Building environment')
+
+
+  time('Initializing Evaluation')
 
   const { stepAll, buildEvaluation } = interpreter(environment, natives as Natives)
-
-  log.start('Initializing Evaluation')
   const baseEvaluation = buildEvaluation()
   stepAll(baseEvaluation)
   baseEvaluation.frameStack.pop()
-  log.done('Initializing Evaluation')
 
-  function registerTests(evaluation: Evaluation, nodes: List<Node>): void {
-    nodes.forEach(node => {
-
-      if (node.is('Describe') || node.is('Package'))
-        describe(node.name, () => registerTests(evaluation, node.members))
-
-      if (node.is('Test'))
-        it(node.name, () => {
-          const { runTest } = interpreter(evaluation.environment, natives as Natives)
-          const { error } = runTest(evaluation.copy(), node)
-          if (error) fail(`${error}`)
-        })
-
-    })
-  }
+  timeEnd('Initializing Evaluation')
 
   registerTests(baseEvaluation, baseEvaluation.environment.members)
 
