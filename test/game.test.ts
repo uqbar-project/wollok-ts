413d669bb678d750a7322ce7686f5cbe87a6c71d
PalumboN <nahuel.palumbo@gmail.com>
2020-01-02 18:13:49 -0300

    Natives tests

--- test/game.test.ts
+++ test/game.test.ts
@@ -29,160 +29,6 @@ describe('Wollok Game', () => {
     visuals(evaluation).should.have.length(0)
   })
 
-  // TODO: Move these to .wtest
-  describe('io events', () => {
-
-    it.skip('addVisualCharacter', () => {
-      runGame(`
-        const character = new Visual()
-        game.addVisualCharacter(character)
-        io.queueEvent(["keydown", "KeyUp"])
-        io.flushEvents(0)
-        assert.equals(game.at(0, 1), character.position())
-      `)
-    })
-
-    it('whenKeyPressedDo', () => {
-      runGame(`
-        const event = ["keydown", "KeyA"]
-        game.whenKeyPressedDo(event, closureMock)
-        io.queueEvent(event)
-        io.flushEvents(0)
-        assert.that(closureMock.called())
-      `)
-    })
-
-    describe('whenCollideDo', () => {
-
-      it('never', () => {
-        runGame(`
-          game.whenCollideDo(visual, closureMock)
-          io.flushEvents(0)
-          assert.notThat(closureMock.called())
-        `)
-      })
-
-      it('once', () => {
-        runGame(`
-          game.whenCollideDo(visual, closureMock)
-          game.addVisual(new Visual())
-          io.flushEvents(0)
-          assert.that(closureMock.called())
-        `)
-      })
-
-      it('many times', () => {
-        runGame(`
-          game.whenCollideDo(visual, closureMock)
-          game.addVisual(new Visual())
-          io.flushEvents(0)
-          io.flushEvents(1)
-          assert.equals(2, closureMock.calledCount())
-        `)
-      })
-    })
-
-    describe('onCollideDo', () => {
-
-      it('never', () => {
-        runGame(`
-          game.onCollideDo(visual, closureMock)
-          io.flushEvents(0)
-          assert.notThat(closureMock.called())
-        `)
-      })
-
-      it('once', () => {
-        runGame(`
-          game.onCollideDo(visual, closureMock)
-          game.addVisual(new Visual())
-          io.flushEvents(0)
-          assert.that(closureMock.called())
-        `)
-      })
-
-      it('only once in same collision', () => {
-        runGame(`
-          game.onCollideDo(visual, closureMock)
-          game.addVisual(new Visual())
-          io.flushEvents(0)
-          io.flushEvents(1)
-          assert.equals(1, closureMock.calledCount())
-        `)
-      })
-
-      it('many times in many collisions', () => {
-        runGame(`
-          game.onCollideDo(visual, closureMock)
-          const collider = new Visual()
-          game.addVisual(collider)
-          io.flushEvents(0)
-          collider.position(game.at(1, 1))
-          io.flushEvents(1)
-          collider.position(game.origin())
-          io.flushEvents(2)
-          assert.equals(2, closureMock.calledCount())
-        `)
-      })
-    })
-
-    describe('onTick', () => {
-
-      it('never', () => {
-        runGame(`
-          game.onTick(1000, "", closureMock)
-          io.flushEvents(999)
-          assert.notThat(closureMock.called())
-        `)
-      })
-
-      it('once', () => {
-        runGame(`
-          game.onTick(1000, "", closureMock)
-          io.flushEvents(1000)
-          assert.that(closureMock.called())
-        `)
-      })
-
-      it('many times', () => {
-        runGame(`
-          game.onTick(1000, "", closureMock)
-          io.flushEvents(1000)
-          io.flushEvents(2000)
-          assert.equals(2, closureMock.calledCount())
-        `)
-      })
-    })
-
-    it('removeTickEvent', () => {
-      runGame(`
-        game.onTick(1000, "event", closureMock)
-        game.removeTickEvent("event")
-        io.flushEvents(1000)
-        assert.notThat(closureMock.called())
-      `)
-    })
-
-    describe('schedule', () => {
-
-      it('once', () => {
-        runGame(`
-          game.schedule(1000, closureMock)
-          io.flushEvents(1000)
-          assert.that(closureMock.called())
-        `)
-      })
-
-      it('only once', () => {
-        runGame(`
-          game.schedule(1000, closureMock)
-          io.flushEvents(1000)
-          io.flushEvents(2000)
-          assert.equals(1, closureMock.calledCount())
-        `)
-      })
-    })
-  })
 })
 
 const visualObject = (evaluation: Evaluation) => evaluation.instance(evaluation.environment.getNodeByFQN('test.game.visual').id)
@@ -199,20 +45,11 @@ const runGame = (content: string = '') => {
   const gameProgram = {
     name: 'test/game.wpgm',
     content: `
-      import wollok.io.*
       import wollok.game.*
 
-      object closureMock {
-        var property calledCount = 0
-        method called() = calledCount > 0
-        method apply(args...) { calledCount += 1 }
+      object visual {
+        method position() = game.origin()
       }
-
-      class Visual {
-        var property position = game.origin()
-      }
-
-      object visual inherits Visual { }
 
       program gameTest {
         game.addVisual(visual)
---
+++ test/natives.ts
@@ -0,0 +1,4 @@
+import { join } from 'path'
+import { runAllTests } from './runner'
+
+runAllTests(join('test', 'game'))
