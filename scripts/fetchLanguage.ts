4d1ed64442204a2300707f1b7753e94e735ecce5
Nicolás Scarcella <nscarcella@gmail.com>
2020-12-21 17:10:14 -0300

    Updating node version

--- scripts/fetchLanguage.ts
+++ scripts/fetchLanguage.ts
@@ -1,9 +1,10 @@
 import { existsSync, mkdirSync } from 'fs'
 import gitClient from 'simple-git/promise'
 import log, { enableLogs, LogLevel } from '../src/log'
+import { wollokVersion } from '../package.json'
 
 const WOLLOK_LANGUAGE_REPO = 'https://github.com/uqbar-project/wollok-language.git'
-const WOLLOK_LANGUAGE_VERSION = process.env.npm_package_wollokVersion
+const WOLLOK_LANGUAGE_TAG = `v${wollokVersion}`
 const WOLLOK_LANGUAGE_FOLDER = 'language'
 
 enableLogs(LogLevel.INFO)
@@ -13,11 +14,11 @@ const fetchLanguage = async () => {
   if (existsSync(WOLLOK_LANGUAGE_FOLDER)) {
     log.info('\tFound local version of Wollok Language!')
   } else {
-    log.info(`\tChecking out the Wollok Language project to ./${WOLLOK_LANGUAGE_FOLDER} from ${WOLLOK_LANGUAGE_REPO} at tag: v${WOLLOK_LANGUAGE_VERSION}`)
+    log.info(`\tChecking out the Wollok Language project to ./${WOLLOK_LANGUAGE_FOLDER} from ${WOLLOK_LANGUAGE_REPO} at tag: ${WOLLOK_LANGUAGE_TAG}`)
     mkdirSync(WOLLOK_LANGUAGE_FOLDER)
     const client = gitClient(WOLLOK_LANGUAGE_FOLDER)
     await client.clone(WOLLOK_LANGUAGE_REPO, '.')
-    await client.checkout(`v${WOLLOK_LANGUAGE_VERSION}`)
+    await client.checkout(WOLLOK_LANGUAGE_TAG)
   }
   log.done('Obtaining the Wollok Language specification')
 }
