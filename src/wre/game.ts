92b353c3b823152f5ab45e76660041defeabe7cb
Ezequiel Pereyra <31800576+ezequielPereyra@users.noreply.github.com>
2021-07-23 17:27:01 -0300

    Deprecating addVisualIn and addVisualCharacterIn

--- src/wre/game.ts
+++ src/wre/game.ts
@@ -14,26 +14,8 @@ const game: Natives = {
       visuals.push(visual)
     },
 
-    *addVisualIn(self: RuntimeObject, visual: RuntimeObject, position: RuntimeObject): Execution<void> {
-      visual.assertIsNotNull()
-      position.assertIsNotNull()
-
-      const visuals = self.get('visuals')?.innerCollection
-      if (!visuals) self.set('visuals', yield* this.list(visual))
-      else {
-        if(visuals.includes(visual)) throw new TypeError(visual.module.fullyQualifiedName())
-        visuals.push(visual)
-      }
-
-      visual.set('position', position)
-    },
-
     *addVisualCharacter(_self: RuntimeObject, visual: RuntimeObject): Execution<RuntimeValue> {
       return yield* this.send('addVisualCharacter', this.object('wollok.gameMirror.gameMirror')!, visual)
-    },
-
-    *addVisualCharacterIn(_self: RuntimeObject, visual: RuntimeObject, position: RuntimeObject): Execution<RuntimeValue> {
-      return yield* this.send('addVisualCharacterIn', this.object('wollok.gameMirror.gameMirror')!, visual, position)
     },
 
     *removeVisual(self: RuntimeObject, visual: RuntimeObject): Execution<void> {
