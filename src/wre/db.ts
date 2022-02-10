b410c3cdae0e5666386dfc429ad252ed9823e710
gomezdn <gomezdn@outlook.com>
2022-02-10 04:59:28 -0300

    db tells singleton from instance (with default attributes yet)

--- src/wre/db.ts
+++ src/wre/db.ts
@@ -13,17 +13,41 @@ const loadDataBase = (): DataBase => {
 const attributesMapToObject = (map: Map<Name, RuntimeValue>) => {
   const object: Attributes = {}
   map.forEach((value, key) => {
-    if (isExpression(value)) {
-      object[key] = { type: 'literal', savedValue: value!.innerValue as SavedValue }
-    } else if (key !== 'self') {
-      object[key] = { type: 'singleton', savedValue: value!.module.fullyQualifiedName() }
+    if (isLiteral(value)) {
+      object[key] = saveAsLiteral(value)
+    }
+    else if (isSingleton(value, key)) {
+      object[key] = saveAsSingleton(value)
+    }
+    else if (isInstance(value, key)) {
+      object[key] = saveAsInstance(value)
     }
   })
   return object
 }
 
-const isExpression = (runtimeValue: RuntimeValue): boolean => {
-  return runtimeValue!.innerValue !== undefined
+const saveAsLiteral = (value: RuntimeValue) => {
+  return { type: 'literal', savedValue: value!.innerValue as SavedValue }
+}
+
+const saveAsSingleton = (value: RuntimeValue) => {
+  return { type: 'singleton', savedValue: value!.module.fullyQualifiedName() }
+}
+
+const saveAsInstance = (value: RuntimeValue) => {
+  return { type: 'instance', savedValue: value!.module.fullyQualifiedName() }
+}
+
+const isLiteral = (value: RuntimeValue) => {
+  return value!.innerValue !== undefined
+}
+
+const isSingleton = (value: RuntimeValue, keyInMap: string) => {
+  return keyInMap !== 'self' && value!.module.kind === 'Singleton'
+}
+
+const isInstance = (value: RuntimeValue, keyInMap: string) => {
+  return keyInMap !== 'self' && value!.module.kind === 'Class'
 }
 
 const saveObjectAttributes = (objName: Name, attributes: Attributes) => {
@@ -35,7 +59,8 @@ const saveObjectAttributes = (objName: Name, attributes: Attributes) => {
 const attributeReviver = (value: SavedValue, evaluation: Evaluation): Record<string, Function> => {
   const literal = () => evaluation.reify(value as SavedValue)
   const singleton = () => evaluation.object(value as Name)
-  return { literal, singleton }
+  const instance = () => evaluation.instantiate(value as Name)
+  return { literal, singleton, instance }
 }
 
 const loadObjectAttributes = (runtimeObject: RuntimeObject, attributes: Attributes, evaluation: Evaluation) => {
