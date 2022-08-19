6803ec3e71da5f7a07fd31f420230fbe94dc8d8f
Nicolás Scarcella <nscarcella@gmail.com>
2022-08-19 01:11:15 -0300

    uncommenting tests

--- test/game.test.ts
+++ test/game.test.ts
@@ -1,57 +1,57 @@
-// import { should } from 'chai'
-// import { join } from 'path'
-// import { buildEnvironment } from './assertions'
-// import natives from '../src/wre/wre.natives'
-// import { Environment } from '../src'
-// import interpret from '../src/interpreter/interpreter'
+import { should } from 'chai'
+import { join } from 'path'
+import { buildEnvironment } from './assertions'
+import natives from '../src/wre/wre.natives'
+import { Environment } from '../src'
+import interpret from '../src/interpreter/interpreter'
 
-// should()
+should()
 
-// // TODO: Move the wollok code to language
+// TODO: Move the wollok code to language
 
-// describe('Wollok Game', () => {
+describe('Wollok Game', () => {
 
-//   describe('actions', () => {
+  describe('actions', () => {
 
-//     let environment: Environment
+    let environment: Environment
 
-//     before(async () => {
-//       environment = await buildEnvironment('**/*.wpgm', join('test', 'game'))
-//     })
+    before(async () => {
+      environment = await buildEnvironment('**/*.wpgm', join('test', 'game'))
+    })
 
-//     it('addVisual', () => {
-//       const interpreter = interpret(environment, natives)
-//       interpreter.run('actions.addVisual')
-//       const visuals = interpreter.object('wollok.game.game').get('visuals')!.innerValue!
-//       visuals.should.have.length(1)
-//     })
+    it('addVisual', () => {
+      const interpreter = interpret(environment, natives)
+      interpreter.run('actions.addVisual')
+      const visuals = interpreter.object('wollok.game.game').get('visuals')!.innerValue!
+      visuals.should.have.length(1)
+    })
 
-//     it('removeVisual', () => {
-//       const interpreter = interpret(environment, natives)
-//       interpreter.run('actions.removeVisual')
-//       const visuals = interpreter.object('wollok.game.game').get('visuals')!.innerValue!
-//       visuals.should.have.length(0)
-//     })
+    it('removeVisual', () => {
+      const interpreter = interpret(environment, natives)
+      interpreter.run('actions.removeVisual')
+      const visuals = interpreter.object('wollok.game.game').get('visuals')!.innerValue!
+      visuals.should.have.length(0)
+    })
 
-//     it('say', () => {
-//       const interpreter = interpret(environment, natives)
-//       interpreter.run('actions.say')
-//       interpreter.object('actions.visual').get('message')!.innerValue!.should.equal('Hi!')
-//       interpreter.object('actions.visual').get('messageTime')!.innerValue!.should.equal(2000)
-//     })
+    it('say', () => {
+      const interpreter = interpret(environment, natives)
+      interpreter.run('actions.say')
+      interpreter.object('actions.visual').get('message')!.innerValue!.should.equal('Hi!')
+      interpreter.object('actions.visual').get('messageTime')!.innerValue!.should.equal(2000)
+    })
 
-//     it('clear', () => {
-//       const interpreter = interpret(environment, natives)
-//       interpreter.run('actions.clear')
-//       const visuals = interpreter.object('wollok.game.game')!.get('visuals')!.innerValue!
-//       visuals.should.have.length(0)
-//     })
+    it('clear', () => {
+      const interpreter = interpret(environment, natives)
+      interpreter.run('actions.clear')
+      const visuals = interpreter.object('wollok.game.game')!.get('visuals')!.innerValue!
+      visuals.should.have.length(0)
+    })
 
-//     it('flush event', () => {
-//       const interpreter = interpret(environment, natives)
-//       const gameMirror = interpreter.object('wollok.gameMirror.gameMirror')!
-//       const time = interpreter.reify(1)
-//       interpreter.send('flushEvents', gameMirror, time)
-//     })
-//   })
-// })
+    it('flush event', () => {
+      const interpreter = interpret(environment, natives)
+      const gameMirror = interpreter.object('wollok.gameMirror.gameMirror')!
+      const time = interpreter.reify(1)
+      interpreter.send('flushEvents', gameMirror, time)
+    })
+  })
+})
--- test/validator.test.ts
+++ test/validator.test.ts
@@ -1,70 +1,70 @@
-// import { Node, Problem } from './../src/model'
-// import { Annotation, buildEnvironment } from '../src'
-// import globby from 'globby'
-// import { readFileSync } from 'fs'
-// import { join } from 'path'
-// import validate from '../src/validator'
-// import { should } from 'chai'
-// import { fail } from 'assert'
-// import { notEmpty } from '../src/extensions'
+import { Node, Problem } from './../src/model'
+import { Annotation, buildEnvironment } from '../src'
+import globby from 'globby'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+import validate from '../src/validator'
+import { should } from 'chai'
+import { fail } from 'assert'
+import { notEmpty } from '../src/extensions'
 
-// const TESTS_PATH = 'language/test/validations'
+const TESTS_PATH = 'language/test/validations'
 
-// should()
+should()
 
-// describe('Wollok Validations', () => {
-//   const files = globby.sync('**/*.@(wlk|wtest|wpgm)', { cwd: TESTS_PATH }).map(name => ({
-//     name,
-//     content: readFileSync(join(TESTS_PATH, name), 'utf8'),
-//   }))
-//   const environment = buildEnvironment(files)
+describe('Wollok Validations', () => {
+  const files = globby.sync('**/*.@(wlk|wtest|wpgm)', { cwd: TESTS_PATH }).map(name => ({
+    name,
+    content: readFileSync(join(TESTS_PATH, name), 'utf8'),
+  }))
+  const environment = buildEnvironment(files)
 
-//   const matchesExpectation = (problem: Problem, expected: Annotation) => {
-//     const code = expected.args.get('code')!
-//     return problem.code === code
-//   }
+  const matchesExpectation = (problem: Problem, expected: Annotation) => {
+    const code = expected.args.get('code')!
+    return problem.code === code
+  }
 
-//   const errorLocation = (node: Node | Problem): string => `${node.sourceMap}`
+  const errorLocation = (node: Node | Problem): string => `${node.sourceMap}`
 
-//   for(const file of files) {
-//     const packageName = file.name.split('.')[0]
+  for(const file of files) {
+    const packageName = file.name.split('.')[0]
 
-//     it(packageName, () => {
-//       const filePackage = environment.getNodeByFQN(packageName)
+    it(packageName, () => {
+      const filePackage = environment.getNodeByFQN(packageName)
 
-//       const nodesWithFileErrors = filePackage.reduce((nodesWithProblems, node) => node.hasProblems ? [...nodesWithProblems, node] : nodesWithProblems, [] as Node[])
-//       if (notEmpty(nodesWithFileErrors))
-//         fail(`Problems in file. ${nodesWithFileErrors.map(node => node.problems![0].code + ' at ' + errorLocation(node))}`)
+      const nodesWithFileErrors = filePackage.reduce((nodesWithProblems, node) => node.hasProblems ? [...nodesWithProblems, node] : nodesWithProblems, [] as Node[])
+      if (notEmpty(nodesWithFileErrors))
+        fail(`Problems in file. ${nodesWithFileErrors.map(node => node.problems![0].code + ' at ' + errorLocation(node))}`)
 
-//       const allProblems = validate(filePackage)
+      const allProblems = validate(filePackage)
 
-//       filePackage.forEach(node => {
-//         const problems = allProblems.filter(_ => _.node === node)
-//         const expectedProblems = node.metadata.filter(_ => _.name === 'Expect')
+      filePackage.forEach(node => {
+        const problems = allProblems.filter(_ => _.node === node)
+        const expectedProblems = node.metadata.filter(_ => _.name === 'Expect')
 
-//         for(const expectedProblem of expectedProblems) {
-//           const code = expectedProblem.args.get('code')!
-//           const level = expectedProblem.args.get('level')
+        for(const expectedProblem of expectedProblems) {
+          const code = expectedProblem.args.get('code')!
+          const level = expectedProblem.args.get('level')
 
-//           if(!code) fail('Missing required "code" argument in @Expect annotation')
+          if(!code) fail('Missing required "code" argument in @Expect annotation')
 
-//           const errors = allProblems.filter(problem => !matchesExpectation(problem, expectedProblem))
-//           if (notEmpty(errors))
-//             fail(`File contains errors: ${errors.map((_error) => _error.code + ' at ' + errorLocation(_error)).join(', ')}`)
+          const errors = allProblems.filter(problem => !matchesExpectation(problem, expectedProblem))
+          if (notEmpty(errors))
+            fail(`File contains errors: ${errors.map((_error) => _error.code + ' at ' + errorLocation(_error)).join(', ')}`)
 
-//           const effectiveProblem = problems.find(problem => matchesExpectation(problem, expectedProblem))
-//           if(!effectiveProblem)
-//             fail(`Missing expected ${code} ${level ?? 'problem'} at ${errorLocation(node)}`)
+          const effectiveProblem = problems.find(problem => matchesExpectation(problem, expectedProblem))
+          if(!effectiveProblem)
+            fail(`Missing expected ${code} ${level ?? 'problem'} at ${errorLocation(node)}`)
 
-//           if(level && effectiveProblem.level !== level)
-//             fail(`Expected ${code} to be ${level} but was ${effectiveProblem.level} at ${errorLocation(node)}`)
-//         }
+          if(level && effectiveProblem.level !== level)
+            fail(`Expected ${code} to be ${level} but was ${effectiveProblem.level} at ${errorLocation(node)}`)
+        }
 
-//         for(const problem of problems) {
-//           if(!expectedProblems.some(expectedProblem => matchesExpectation(problem, expectedProblem)))
-//             fail(`Unexpected ${problem.code} ${problem.level} at ${errorLocation(node)}`)
-//         }
-//       })
-//     })
-//   }
-// })
+        for(const problem of problems) {
+          if(!expectedProblems.some(expectedProblem => matchesExpectation(problem, expectedProblem)))
+            fail(`Unexpected ${problem.code} ${problem.level} at ${errorLocation(node)}`)
+        }
+      })
+    })
+  }
+})
