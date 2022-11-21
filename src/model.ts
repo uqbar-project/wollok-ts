52b45ca336a4a21ab54c19d6584fd3e353de6ae5
ivojawer <ivojawerbaum@gmail.com>
2022-11-20 22:22:43 -0300

    cond -> closure

--- src/model.ts
+++ src/model.ts
@@ -255,9 +255,9 @@ abstract class $Node {
     return applyReduce(initial, this)
   }
 
-  filter(this: Node, cond: (node: Node, parent?: Node) => boolean): Node[] {
+  filter(this: Node, closure: (node: Node, parent?: Node) => boolean): Node[] {
     return this.reduce<Node[]>((acum, node, parent) => {
-      return cond(node, parent) ? acum.concat(node) : acum
+      return closure(node, parent) ? acum.concat(node) : acum
     }, [])
   }
 
