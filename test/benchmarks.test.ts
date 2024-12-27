1cee6c31ad4801880c1fb08bacb2f1ebc85d08ee
palumbon <nahuel.palumbo@gmail.com>
2024-12-27 17:07:45 -0300

    Improving game natives for performance

--- test/benchmarks.test.ts
+++ test/benchmarks.test.ts
@@ -1,7 +1,7 @@
 import { should } from 'chai'
 import { resolve } from 'path'
-import { Environment, Execution, get, Natives, PROGRAM_FILE_EXTENSION, RuntimeObject } from '../src'
-import { interpret, interprete, Interpreter } from '../src/interpreter/interpreter'
+import { Environment, PROGRAM_FILE_EXTENSION } from '../src'
+import { interpret, Interpreter } from '../src/interpreter/interpreter'
 import natives from '../src/wre/wre.natives'
 import { buildEnvironment } from './assertions'
 
